f8d609d629d3d54039055b60830ba9b6635bc029
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2020-09-26 23:54:12 +0200

    front-end completed

---
+++ images/js.js
@@ -0,0 +1,12 @@
+var menu_item = document.getElementById("menu_item"){
+    menu_item.style.maxHeight = "0px";
+function menu_icon_toggle() {
+    if (menu_item.style.maxHeight == "0px") {
+        menu_item.style.maxHeight = "200px";
+
+
+    } else {
+        menu_item.style.maxHeight = "0px";
+    }
+
+}
